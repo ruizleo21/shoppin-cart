643feb7625678d9b076f4365109888e6919501f4
Leonardo <ruizleo@ingenieros.com>
2021-07-13 19:58:11 -0500

    fix cors

--- src/main/java/co/com/experis/controller/ProductController.java
+++ src/main/java/co/com/experis/controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +29,7 @@ import co.com.experis.service.ProductService;
  */
 @RestController
 @RequestMapping("/product")
+@CrossOrigin("*")
 public class ProductController {
 
 	Logger logger = LoggerFactory.getLogger(ProductController.class);
--- src/main/java/co/com/experis/controller/ShoppingCarController.java
+++ src/main/java/co/com/experis/controller/ShoppingCarController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ import co.com.experis.service.ShoppingCarService;
 
 @RestController
 @RequestMapping("/shopping-cart")
+@CrossOrigin("*")
 public class ShoppingCarController {
 	
 	Logger logger = LoggerFactory.getLogger(ShoppingCarController.class);
