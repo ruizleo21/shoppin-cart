e402c2f500442ef332e81170674e544e8a28d985
Leonardo <ruizleo@ingenieros.com>
2021-07-13 19:55:27 -0500

    fix cors

--- src/main/java/co/com/experis/ShoppingCarApplication.java
+++ src/main/java/co/com/experis/ShoppingCarApplication.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import co.com.experis.service.ProductService;
 
@@ -29,6 +31,16 @@ public class ShoppingCarApplication {
 	@PostConstruct
 	public void loadFile() {
 		productService.loadFile("Productos.csv");
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
 	}
 
 }
